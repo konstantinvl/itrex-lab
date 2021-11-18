e88d9077ef2066ec5be9ec78b9db80a1a8f52d5b
konstantinvl <konstantinvl@inbox.ru>
2021-11-19 02:17:44 +0300

    feat: add patient newAppointment page

---
+++ src/pages/view/elements/styles/appointmentRadioWrapper.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const RadioWrapper = styled.fieldset`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: flex-start;
+    border: none;
+    margin-top: 24px;
+    padding: 0;
+`;
+
+export default RadioWrapper;
